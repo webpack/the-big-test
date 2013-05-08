1aa0dec58149e9f19888fd5641c7da7dda0f7fcf
Tobias Koppers <tobias.koppers@googlemail.com>
2013-05-08 15:04:16 +0200

    fixed cover test

--- cover-webpack.config.js
+++ cover-webpack.config.js
@@ -3,7 +3,8 @@ module.exports = {
 	module: {
 		loaders: [
 			{ test: /\.json$/, loader: "json" },
-			{ test: /\.css$/, loader: "style!css" }
+			{ test: /\.css$/, loader: "style!css" },
+			{ test: /\.coffee$/, loader: "coffee-loader" }
 		],
 		postLoaders: [{
 			test: /./, // any
@@ -11,7 +12,8 @@ module.exports = {
 				/node_modules.chai/,
 				/node_modules.mocha-loader/,
 				/node_modules.coverjs-loader/,
-				/node_modules.webpack.buildin/
+				/node_modules.webpack.buildin/,
+				/node_modules.node-libs-browser/
 			],
 			loader: "coverjs-loader"
 		}]
